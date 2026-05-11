604f2e7855f362ec9f04a998e498479af127196b
Duygukaplanlar <dkaplanlar2@gmail.com>
2026-05-11 23:28:53 +0300

    CartItem.cs

--- CartItem.cs
+++ CartItem.cs
@@ -7,7 +7,7 @@
         public decimal Price { get; set; }
         public decimal? DiscountPrice { get; set; }
         public int Quantity { get; set; }
-        // Toplam fiyatı otomatik hesaplayan özellik
+        
         public decimal TotalPrice => Price * Quantity;
     }
 }
