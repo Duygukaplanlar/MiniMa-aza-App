890553afb715b6c1505f55d8a72dac08bc9a22c9
Duygukaplanlar <dkaplanlar2@gmail.com>
2026-05-11 23:32:32 +0300

    ProductController.cs

--- ProductController.cs
+++ ProductController.cs
@@ -14,13 +14,13 @@ namespace MiniMagaza.Controllers
             _context = context;
         }
 
-        // 1. Ürün Listeleme (Anasayfa)
+        
         public async Task<IActionResult> Index(int? categoryId)
         {
-            // Veritabanındaki tüm ürünleri getir ama Kategorilerini de içine dahil et (Include)
+           
             var urunlerQuery = _context.Products.Include(p => p.Category).AsQueryable();
 
-            // Eğer bir kategoriye tıklandıysa (categoryId dolu geldiyse) listeyi filtrele
+           
             if (categoryId.HasValue)
             {
                 urunlerQuery = urunlerQuery.Where(p => p.CategoryId == categoryId.Value);
@@ -29,10 +29,10 @@ namespace MiniMagaza.Controllers
             return View(await urunlerQuery.ToListAsync());
         }
 
-        // 2. Ürün Detay Sayfası
+       
         public async Task<IActionResult> Details(int id)
         {
-            // ID'ye göre ürünü bulur, yoksa 404 hatası verir
+            
             var product = await _context.Products.Include(p => p.Category).FirstOrDefaultAsync(m => m.Id == id);
 
             if (product == null)
