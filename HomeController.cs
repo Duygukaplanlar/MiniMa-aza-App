336d79deadfe2125ac0e7532586dffadaaa81067
Duygukaplanlar <dkaplanlar2@gmail.com>
2026-05-11 23:30:27 +0300

    HomeController.cs

--- HomeController.cs
+++ HomeController.cs
@@ -14,13 +14,12 @@ namespace MiniMagaza.Controllers
             _context = context;
         }
 
-        // Ana Sayfa ve Kategori Filtreleme
+        
         public IActionResult Index(int? categoryId)
         {
-            // Veritabanından ürünleri çekiyoruz
+            
             var urunlerQuery = _context.Products.AsQueryable();
 
-            // Eğer bir kategori seçildiyse ona göre filtrele
             if (categoryId.HasValue)
             {
                 urunlerQuery = urunlerQuery.Where(p => p.CategoryId == categoryId);
@@ -30,10 +29,10 @@ namespace MiniMagaza.Controllers
             return View(model);
         }
 
-        // Ürün Detay Sayfası
+   
         public IActionResult Details(int id)
         {
-            // ID'ye göre ilgili ürünü bul
+            
             var urun = _context.Products.FirstOrDefault(p => p.Id == id);
 
             if (urun == null)
